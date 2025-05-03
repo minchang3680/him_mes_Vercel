c99e763946e4c171acdfe339291e7c176624a4a7
minchang3680 <minchang3680@naver.com>
2025-05-03 11:14:07 +0900

    fix: use .eslintrc.js for Vercel compatibility

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: ['next/core-web-vitals', 'next'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  };
+  
