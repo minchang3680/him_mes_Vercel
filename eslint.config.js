39662de559321cebc25937d7bc2816594e8736af
minchang3680 <minchang3680@naver.com>
2025-05-03 12:17:56 +0900

    좀 되라

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+    extends: ['next', 'next/core-web-vitals'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  };
+  
