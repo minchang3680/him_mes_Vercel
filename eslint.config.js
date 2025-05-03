f6c15edb1ad986013b3b9aa6d82e084221fcb39e
minchang3680 <minchang3680@naver.com>
2025-05-03 11:47:00 +0900

    fix: Vercel ESLint 설정을 eslint.config.js로 이관

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
+// eslint.config.js
 module.exports = {
     root: true,
-    extends: ['next'],
+    extends: ['next', 'next/core-web-vitals', 'eslint:recommended'],
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
