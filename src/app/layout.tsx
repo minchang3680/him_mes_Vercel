8acbf190de45a735e1658959e4a0a5c9931f1b9c
minchang3680 <minchang3680@naver.com>
2025-05-10 10:51:59 +0900

    타이틀 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -12,11 +13,16 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+export const metadata: Metadata = {
+  title: "스마트팩토리 MES 대시보드",
+  description: "MES 기반의 진단 시각화 플랫폼입니다.",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) { 
   return (
     <html lang="en">
       <body
@@ -27,3 +33,6 @@ export default function RootLayout({
     </html>
   );
 }
+
+
+
